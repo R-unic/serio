21fdd9908c511e68466f6e5e456dd92cdfa6dd30
R-unic <roleyspersonal@gmail.com>
2026-01-06 10:33:10 -0500

    build: test coverage

--- tests/src/run-tests.server.ts
+++ tests/src/run-tests.server.ts
@@ -1,4 +1,17 @@
 import { TestRunner } from "@rbxts/runit";
+import { instrument, istanbul } from "@rbxts/coverage";
 
-const testRunner = new TestRunner(game.GetService("ReplicatedStorage").WaitForChild("Tests"));
-testRunner.run({ colors: true }).await();
+const http = game.GetService("HttpService");
+const replicated = game.GetService("ReplicatedStorage");
+instrument([replicated.WaitForChild("Library")]);
+
+const testRunner = new TestRunner(replicated.WaitForChild("Tests"));
+testRunner.run({ colors: true })
+  .then(() => {
+    const report = istanbul();
+    const reportJSON = http.JSONEncode(report);
+    const coverageValue = new Instance("StringValue");
+    coverageValue.Name = "coverage";
+    coverageValue.Value = reportJSON;
+    coverageValue.Parent = replicated;
+  });
